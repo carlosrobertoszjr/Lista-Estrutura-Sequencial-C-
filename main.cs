9a524fdba0d2e50e85b3e7b65fd410077bb685a4
Carlos Roberto <carlosrobertoszjr@gmail.com>
2020-08-21 16:55:40 +0000

    Exercício completo

--- main.cs
+++ main.cs
@@ -1,19 +1,54 @@
 using System;
-//Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 class MainClass {
 
-  public static void ExibeNumero (float numero) {
-    Console.WriteLine (" O número informado foi {0}", numero);
+// 3) SOMA DE DOIS número
+
+  public static void Exerc03 (float num1, float num2) {
+    Console.WriteLine (" A soma dos números é {0,1}", num1+num2);
     }
 
-  //COORDENAÇÃO
   public static void Main () {
-    float numero;
-    Console.Write("Digite um número >> ");
+    float num1;
+    float num2;
+  
     //TYPECAST
-    numero = float.Parse(Console.ReadLine());
-    ExibeNumero(numero);
+    Console.Write("Digite o primeiro número >> ");
+    num1 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o segundo número >> ");
+    num2 = float.Parse(Console.ReadLine());
+    Exerc03(num1,num2);
     
   }
 
+
+
+// 4) Faça um Programa que peça as 4 notas bimestrais e mostre a média
+ 
+class MainClass {
+  public static void Exerc04 (float num1, float num2, float num3, float num4, float media) {
+    Console.WriteLine("A média das notas é " + media);
+    }
+
+  public static void Main () {
+    float num1;
+    float num2;
+    float num3;
+    float num4;
+    
+
+  
+    //TYPECAST
+    Console.Write("Digite o primeiro número >> ");
+    num1 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o segundo número >> ");
+    num2 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o terceiro número >> ");
+    num3 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o quarto número >> ");
+    num4 = float.Parse(Console.ReadLine());
+    float media = ((num1+num2+num3+num4) / 4);
+    Exerc04(num1,num2,num3,num4,media);
+    
+  }
+    
 }
